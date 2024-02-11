8f18ee520a25a93b82f5556f6328e2ab500547cc
S-Filip <100999946+S-Filip@users.noreply.github.com>
2024-02-11 08:46:10 +0100

    Setting up base
    Basic logic for the app

---
+++ app.py
@@ -0,0 +1,17 @@
+#filename = "key_file.txt"
+#data_to_write = input("Enter a key (1-25) for your Caesar cipher.")
+#
+#with open(filename, "w") as f:
+#   f.write(data_to_write)
+#
+#print(f"File {filename} created successfully.")
+
+
+choice = input("Press 1 to set the key\nPress 2 to encrypt a message\n Press 3 to decrypt a message")
+match choice:
+    case 1:
+        print()
+    case 2:
+        print()
+    case 3:
+        print()
