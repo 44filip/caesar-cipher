17b575a86f81108e61cde3e64cce0ca84c615152
S-Filip <100999946+S-Filip@users.noreply.github.com>
2024-02-11 08:56:06 +0100

    Writing key file functionality
    Making the logic for file writing and error catching

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-#filename = "key_file.txt"
-#data_to_write = input("Enter a key (1-25) for your Caesar cipher.")
-#
-#with open(filename, "w") as f:
-#   f.write(data_to_write)
-#
-#print(f"File {filename} created successfully.")
-
-
-choice = input("Press 1 to set the key\nPress 2 to encrypt a message\n Press 3 to decrypt a message")
+choice = input("Press 1 to set the key\nPress 2 to encrypt a message\nPress 3 to decrypt a message")
 match choice:
-    case 1:
+    case "1":
+        filename = "key_file.txt"
+        data_to_write = None
+        
+        while isinstance(data_to_write, int) is False or not (1 <= data_to_write <= 25):
+            try:
+                data_to_write = int(input("Enter a key (1-25) for your Caesar cipher."))
+            except ValueError:
+                print("Invalid input. Please enter an integer.")
+        
+        with open(filename, "w") as f:
+           f.write(str(data_to_write))
+        
+        print(f"File {filename} created successfully.")
+    case "2":
         print()
-    case 2:
-        print()
-    case 3:
+    case "3":
         print()
