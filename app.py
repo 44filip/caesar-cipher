c8ad2dd5ae4f87d30e0aeea259be7dce53109520
S-Filip <100999946+S-Filip@users.noreply.github.com>
2024-02-11 09:05:34 +0100

    Multiple functions, fixing logic
    Separating main from set_key, and fixing the logic indentation issues

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-choice = input("Press 1 to set the key\nPress 2 to encrypt a message\nPress 3 to decrypt a message")
-match choice:
-    case "1":
-        filename = "key_file.txt"
-        data_to_write = None
+def main():
+    while True:
+        choice = input("1. Set the key\n2. Encrypt a message\n3. Decrypt a message\n4. Exit\n\nEnter your choice: ")
+        match choice:
+            case "1":
+                set_key()
+            case "2":
+                print()
+            case "3":
+                print()
+            case "4":
+                print("Exiting the program...")
+                break
+            case _:
+                print("Invalid choice.\n")
         
-        while isinstance(data_to_write, int) is False or not (1 <= data_to_write <= 25):
-            try:
-                data_to_write = int(input("Enter a key (1-25) for your Caesar cipher."))
-            except ValueError:
-                print("Invalid input. Please enter an integer.")
+def set_key():
+    filename = "key_file.txt"
+    data_to_write = None
         
-        with open(filename, "w") as f:
+    while isinstance(data_to_write, int) is False or not (1 <= data_to_write <= 25):
+        try:
+            data_to_write = int(input("Enter a key (1-25) for your Caesar cipher: "))
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+        
+    with open(filename, "w") as f:
            f.write(str(data_to_write))
         
-        print(f"File {filename} created successfully.")
-    case "2":
-        print()
-    case "3":
-        print()
+    print(f"File {filename} created successfully.\n")
+        
+if __name__ == "__main__":
+    main()
